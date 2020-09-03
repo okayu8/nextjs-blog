1da143704b46d74f59ecea65e572d6e0714a9b6d
片岡祐太郎 <hitugaya12@gmail.com>
2020-09-03 19:51:33 +0900

    .prettierrcの追加

--- next.config.js
+++ next.config.js
@@ -1,10 +1,10 @@
-const path = require("path");
+const path = require('path');
 
 module.exports = {
   webpack: (config) => {
     config.resolve.alias = {
       ...config.resolve.alias,
-      "~": path.resolve(__dirname, "./src"),
+      '~': path.resolve(__dirname, './src'),
     };
     return config;
   },
