82c411770cc78c1953f404e5afb504f82f9b2bfd
okayu8 <hitugaya12@gmail.com>
2021-07-18 21:33:23 +0900

    some more header style

--- src/components/CommonHeader.tsx
+++ src/components/CommonHeader.tsx
@@ -13,6 +13,8 @@ const Wrapper = styled.header`
   width: 100%;
   height: ${Const.SIZE.HEIGHT.HEADER}px;
   background-color: ${Const.COLOR.BACKGROUND.HEADER};
+  box-shadow: 0px 4px 5px 0px rgb(0 0 0 / 14%), 0px 1px 10px 0px rgb(0 0 0 / 12%),
+    0px 2px 4px -1px rgb(0 0 0 / 20%);
 `
 
 const Inner = styled.div`
